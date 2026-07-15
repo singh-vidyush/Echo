ecc220b3fc9783deaac5113e888fac24fc970845
singh-vidyush <singhvidyush@gmail.com>
2026-07-15 06:49:41 +0000

    vectorization updated

--- src/vectorization.py
+++ src/vectorization.py
@@ -2,21 +2,23 @@ from pathlib import Path
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_chroma import Chroma
 
-class Embedding:
-    def __init__(self, embedding_path = 'chroma_db'):
-        self.embedding_path = embedding_path
+class Vectorization:
+    def __init__(self, chunk):
+        self.chunk = chunk
+        self.embedding_path = 'chroma_db'
+        self.embedding_model = HuggingFaceEmbeddings(model_name= "sentence-transformers/all-MiniLM-L6-v2")
 
-    def chunk_embedding(self, chunk, embedding_model):
+    def chunk_vectorization(self):
         if Path(self.embedding_path).exists():
             vector_db = Chroma(
                 persist_directory = self.embedding_path,
-                embedding_function = embedding_model
+                embedding_function = self.embedding_model
             )
         else:
             vector_db = Chroma.from_documents(
-                documents = chunk,
+                documents = self.chunk,
                 persist_directory = self.embedding_path,
-                embedding = embedding_model,
+                embedding = self.embedding_model,
             )
 
         return vector_db
